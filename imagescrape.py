ec759e9ad1d7c136decec5f18abeed80acb89403
VinChuayw <106131484+VinChuayw@users.noreply.github.com>
2023-05-18 21:30:41 +0800

    Updated code

--- imagescrape.py
+++ imagescrape.py
@@ -4,11 +4,13 @@ from lxml import etree
 import io
 import re
 
+'''this is a tool for bunkr archives, and other similar mirrors'''
+
 HEADERS = ({'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36', 'Accept-Language': 'en-US, en;q=0.5'})
-site1 = ""
-site2 = ""
-site3 = ""
-site4 = ""
+site1 = "insert URL"
+site2 = "insert URL"
+site3 = "insert URL"
+site4 = "insert URL"
 sites = [site1,site2,site3,site4]
 dlist1, dlist2, dlist3, dlist4 = [], [], [], []
 dlists = [dlist1, dlist2, dlist3, dlist4]
@@ -24,8 +26,6 @@ for site in sites:
     smalllinks = soup.find_all("a")
     links = [a["href"] for a in smalllinks if a["href"].endswith((".jpg", "JPG"))]
     for image in links:
-        if 'å°é¢-8pVSbrZC' in image:
-            continue
         updated_image = re.sub(r'cdn(\d+)', r'i\g<1>', image)
         dlists[count].append(updated_image)
     count += 1
